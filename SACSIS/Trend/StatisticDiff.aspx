07aa111bbda40e621d0382d9bcbfab75ff65b2b5
bfy <baifeiyue1989@yeah.net>
2014-03-06 18:38:26 +0800

    修改：白飞跃
    修改时间：2014-03-06 18:37
    修改内容： 统计标杆风机与场站实际发电量数据对比功能

---
+++ SACSIS/Trend/StatisticDiff.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StatisticDiff.aspx.cs" Inherits="SACSIS.Trend.StatisticDiff" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>统计标杆风机理论发电量与场站实际发电量</title>
+     <link href="../js/My97DatePicker/skin/WdatePicker.css" rel="stylesheet" type="text/css" />
+    <link href="../Js/jQueryEasyUI/themes/default/easyui.css" rel="stylesheet" type="text/css" />
+    <link href="../Js/jQueryEasyUI/themes/icon.css" rel="stylesheet" type="text/css" />
+    <script src="../js/My97DatePicker/WdatePicker.js" type="text/javascript"></script>
+    <script src="../Js/jquery.easyui.min.js" type="text/javascript"></script>
+    <script src="../Js/jquery-1.9.1.js" type="text/javascript"></script>
+    <script src="../Js/jquery-1.8.2.min.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        /*初始化数据*/
+        function Init() {
+            $.post("StatisticDiff.aspx", { param: 'Init' }, function (data) {
+                var lists = $.parseJSON(data).Company;
+                $("#FValue").empty();
+                if (lists != null) {
+                    for (var i = 0; i < lists.length; i++) {
+                        $("#FValue").append("<option value='" + lists[i].ID + "'>" + lists[i].NAME + "</option>");
+                    }
+                } else {
+                    $("#FValue").append("<option value='0'>没有分公司数据</option>");
+                }
+
+                lists = $.parseJSON(data).WindStation;
+                $("#CValue").empty();
+                if (lists != null) {
+                    for (var i = 0; i < lists.length; i++) {
+                        $("#CValue").append("<option value='" + lists[i].ID + "'>" + lists[i].NAME + "</option>");
+                    }
+                } else {
+                    $("#CValue").append("<option value='0'>没有风场数据</option>");
+                }
+            })
+        }
+
+
+
+        $(function () {
+            Init();
+            //选择分公司
+            $("#FValue").change(function () {
+                $.post("StatisticDiff.aspx", {
+                    param: 'org',
+                    id: $("#FValue").val()
+                }, function (data) {
+                    //加载风场
+                    var lists = data.WindStation;
+                    $("#CValue").empty();
+                    if (lists != null && lists.length > 0) {
+                        for (var i = 0; i < lists.length; i++) {
+                            $("#CValue").append("<option value='" + lists[i].ID + "'>" + lists[i].NAME + "</option>");
+                        }
+                    } else {
+                        $("#CValue").append("<option value='0'>没有风场数据</option>");
+                    }
+                }, 'json');
+            });
+        });
+       
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <div style="float: left">
+                &nbsp;&nbsp;&nbsp;公&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;司
+                <select id="FValue" style="width: 120px; text-align: center;">
+                </select>
+            </div>
+             <div style="float: left">
+                &nbsp;&nbsp;&nbsp;风&nbsp;&nbsp;&nbsp;&nbsp;场&nbsp;
+                <select id="CValue" style="width: 130px; text-align: center;">
+                </select>
+            </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SACSIS/Trend/StatisticDiff.aspx.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Collections;
+using BLL;
+using System.Text;
+
+namespace SACSIS.Trend
+{
+    public partial class StatisticDiff : System.Web.UI.Page
+    {
+        protected PeriodBLL _pd = new PeriodBLL();
+        protected Hashtable _ht = null;
+
+        protected WppBLL _wd = new WppBLL();
+        protected string result = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string param = Request["param"];
+            if (param != "")
+            {
+
+                lock (this)
+                {
+                    if (param == "Init")
+                    {
+                        GetInit();
+                    }
+                    //else if (param == "unit")
+                    //{
+                    //    string id = Request["id"];
+                    //    GetUnit(id);
+                    //}
+                    //else if (param == "lineyear")
+                    //{
+                    //    string time = Request["time"];    //查询时间
+                    //    string zType = Request["zType"];  //指标类型
+                    //    string tType = Request["tTtype"]; //时间类型
+                    //    string id = HttpUtility.UrlDecode(Request["id"]);        //机组编号
+                    //    string name = HttpUtility.UrlDecode(Request["name"]);    //机组名称
+                    //    string gq = Request["gq"];                               //工期
+                    //    ShowLineYear(id, zType, tType, time, gq);
+                    //}
+
+                    else if (param == "org")
+                    {
+                        string id = Request["id"];
+                        GetOrgName(id);
+                    }
+                    //else if (param == "gq")
+                    //{
+                    //    string id = Request["id"];
+                    //    GetGQName(id);
+                    //}
+                }
+            }
+
+            //txtYear.Value = DateTime.Now.ToString("yyyy");
+            //txtMonth.Value = DateTime.Now.ToString("yyyy-MM");
+            //txtDay.Value = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+        #region 获取分公司，风场信息
+        protected void GetInit()
+        {
+            DataTable _dtCompany = _wd.dtGetCompany();
+
+            IList<Hashtable> _company = new List<Hashtable>();  //公司
+            IList<Hashtable> _fgs = new List<Hashtable>();       //分公司
+            IList<Hashtable> _fc = new List<Hashtable>();       //风场
+            int a = 0;
+
+            if (_dtCompany.Rows.Count > 0)
+            {
+                for (int i = 0; i < _dtCompany.Rows.Count; i++)
+                {
+                    _ht = new Hashtable();
+                    _ht.Add("ID", _dtCompany.Rows[i]["T_COMID"].ToString());     //公司编码
+                    _ht.Add("NAME", _dtCompany.Rows[i]["T_COMDESC"].ToString());   //公司名称
+                    _company.Add(_ht);
+                }
+
+                string _companyId = _dtCompany.Rows[0]["T_COMID"].ToString();
+                _fc = _pd.GetPeriod(_companyId);
+                DataTable _dtOrg = _wd.GetOrg(_companyId);
+                if (_dtOrg.Rows.Count > 0)
+                {
+                    for (int j = 0; j < _dtOrg.Rows.Count; j++)
+                    {
+                        _ht = new Hashtable();
+                        _ht.Add("ID", _dtOrg.Rows[j]["T_ORGID"].ToString());     //风场编码
+                        _ht.Add("NAME", _dtOrg.Rows[j]["T_ORGDESC"].ToString());   //风场名称
+                        _fgs.Add(_ht);
+                    }
+                    string _orgid = _dtOrg.Rows[0]["T_ORGID"].ToString();
+                    _fc = _wd.GetPeriod(_orgid);
+                }
+
+                //if (_fc != null)
+                //{
+                //    _ht = new Hashtable();
+                //    _ht = _fc[0];
+
+                //    string _pname = _ht["T_PERIODDESC"].ToString();
+                //    if (_pname.Equals("全部"))
+                //    {
+                //        a = 1;
+                //    }
+
+                //    string _pid = _ht["T_PERIODID"].ToString();
+                   
+                //}
+            }
+
+             
+            object obj = new
+            {
+                Company = _company,
+                WindStation = _fgs
+            };
+            result = JsonConvert.SerializeObject(obj);
+            Response.Write(result);
+            Response.End();
+
+        }
+        #endregion
+
+        #region 根据分公司获取场站信息
+        /// <summary>
+        /// 根据分公司获取场站信息
+        /// </summary>
+        /// <param name="companyID">分公司标识</param>
+        protected void GetOrgName(string companyID)
+        {
+            IList<Hashtable> listOrg = new List<Hashtable>();
+            DataTable _dtOrg = _wd.GetOrg(companyID);
+            if (_dtOrg.Rows.Count > 0)
+            {
+                for (int j = 0; j < _dtOrg.Rows.Count; j++)
+                {
+                    _ht = new Hashtable();
+                    _ht.Add("ID", _dtOrg.Rows[j]["T_ORGID"].ToString());     //风场编码
+                    _ht.Add("NAME", _dtOrg.Rows[j]["T_ORGDESC"].ToString());   //风场名称
+                    listOrg.Add(_ht);
+                }
+            }
+            object obj = new
+            {
+                WindStation = listOrg,
+            };
+            result = JsonConvert.SerializeObject(obj);
+            Response.Write(result);
+            Response.End();
+        }
+
+        #endregion
+    }
+}
