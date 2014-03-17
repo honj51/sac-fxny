b16f123024d1d80d523c878b91221a2baed40bdf
bfy <baifeiyue1989@yeah.net>
2014-03-17 15:14:36 +0800

    修改：白飞跃
    修改时间：2014-03-17 15:14
    修改内容： 首页风电负荷去除坏点

--- SACSIS/MainConnect.aspx.cs
+++ SACSIS/MainConnect.aspx.cs
@@ -93,7 +93,7 @@ namespace WebApplication2
             fh =Session["fd"]==null?GetChartsValues("'风电'"): (ArrayList)Session["fd"];
             foreach(var value in fh)
             {
-                if (double.Parse(value.ToString()) < 210)
+                if (double.Parse(value.ToString()) < 2100)
                 {
                     tmp += double.Parse(value.ToString());
                     count++;
